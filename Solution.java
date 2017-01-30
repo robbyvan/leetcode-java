d8226b90dc0cc49e4c6a0cee64b1cb820e3e5fc9
robbyvan <fzpinus@gmail.com>
2017-01-30 15:55:52 -0800

    231

--- Solution.java
+++ Solution.java
@@ -1,24 +1,19 @@
-import java.util.*;
 public class Solution {
-  public int lengthOfLongestSubstring(String s) {
-    Map<Character, Integer> myMap = new HashMap<Character, Integer>();
-    int currhead = 0;
-    int maxLen = 0;
-    int currLen = 0;
+  public boolean isUgly(int num) {
+    while (num % 2 == 0){
+      num = num / 2;
+    }
+    while (num % 3 == 0){
+      num = num / 3;
+    }
+    while (num % 5 == 0){
+      num = num / 5;
+    }
 
-    for (int i = 0; i < s.length(); ++i){
-      char c = s.charAt(i);
-      if (myMap.containsKey(c) == true){
-        currhead = Math.max(myMap.get(c), currhead);//利用max消除错误判断重复
-        currLen = i - currhead;
-      }else{
-        currLen = currLen + 1;
-      }
-      if (currLen > maxLen){
-        maxLen = currLen;
-      }
-      myMap.put(c, i);
-    } 
-    return maxLen;
+    if (num == 1 || num == -1){
+      return false;
+    }else{
+      return true;
+    }
   }
 }
